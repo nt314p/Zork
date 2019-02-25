8f3ea163a892ddc5e76f5d476f988159a871f271
Ben Merbaum <46571024+bmerbaum@users.noreply.github.com>
2019-02-25 17:00:15 -0500

    toString and inventory
    Updated printf to string, toArrayList method, and more on processing duplicate items.

--- Zork/src/com/bayviewglen/zork/Inventory.java
+++ Zork/src/com/bayviewglen/zork/Inventory.java
@@ -6,15 +6,31 @@ import java.util.List;
 
 public class Inventory {
 
-	private static List<Item> items = new ArrayList<Item>();
+	private List<Item> items;
 
 	public Inventory() {
 		items = new ArrayList<Item>();
 	}
+
+	/**
+	 * formats an inventory list as an arrayList
+	 * @param list the list you want to turn into an arrayList
+	 * @return the finished arrayList
+	 */
+	public ArrayList<Item> toArrayList(Inventory list) {
+		ArrayList<Item> temp = new ArrayList<Item>();
+
+		for (int i = 0; i < list.size(); i++) {
+			temp.add(new Item(list.get(i)));
+		}
+
+		return temp;
+	}
+
 	/**
 	 * sort the inventory list
 	 */
-	public static void sort() {
+	public void sort() {
 		Collections.sort(items);
 	}
 
@@ -23,7 +39,7 @@ public class Inventory {
 	 * @param target the item you are looking for in the list
 	 * @return the first occurrence of the item
 	 */
-	public static int getIndexOf(Item target) {
+	public int getIndexOf(Item target) {
 		return items.indexOf(target);
 	}
 
@@ -32,7 +48,7 @@ public class Inventory {
 	 * @param target the item you are looking for in the list
 	 * @return the number of occurrences of target
 	 */
-	public static int getNumMultiples(Item target) {
+	public int getNumMultiples(Item target) {
 		int count = 0;
 
 		for (Item i : items) {
@@ -47,103 +63,139 @@ public class Inventory {
 	 * @param target the item you are looking for in the list
 	 * @return if the item is in the inventory
 	 */
-	public static boolean isInInventory(Item target) {
+	public boolean contains(Item target) {
 		return getIndexOf(target) != -1;
 	}
 
 	/**
-	 * puts the inventory in a clean, neat, organized string like so: ...  
-	 * Bird(5x): 5lbs each
-	 * Pile of leaves(2x): 10lbs each
+	 * Checks if all items in the list are found in the inventory
+	 * 
+	 * @param list inventory-type list of items
+	 * @return if contains all items in the list
+	 */
+	public boolean containsAll(Inventory list) {
+		for (int i = 0; i < list.size(); i++) {
+			if (!items.contains(list.get(i)))
+				return false;
+		}
+		return true;
+	}
+
+	/**
+	 * get method
+	 * 
+	 * @param index the index you want to extract an item from
+	 * @return the reference of the item at the index
+	 */
+	public Item get(int index) {
+		return items.get(index);
+	}
+
+	/**
+	 * puts the inventory in a clean, neat, organized string like so: ... Bird(5x):
+	 * 5lbs each Pile of leaves(2x): 10lbs each
 	 */
 	public String toString() {
 		String result = "";
 
-		for (Item i : items) {
-			result += i.getItemName();
-			if (getNumMultiples(i) > 1)
-				result += "(" + getNumMultiples(i) + "x)";
-			result += ": " + i.getItemWeight() + "lbs";
-			if (getNumMultiples(i) > 1)
-				result += " each";
-			result += ".";
+		for(int i = 0; i<items.size(); i++) {
+			Item j = items.get(i);
+			int num = getNumMultiples(j);
+			
+			String itemName = j.getItemName();
+			String multiples = num>1 ? "(" + getNumMultiples(j) + "x):" : ":";
+			String weight = String.format("%.2f kg", j.getItemWeight());
+			String each = num>1? " each" : "";
+			
+			String part1 = itemName + multiples;
+			String part2 = weight + each;
+			
+			result += String.format("%-15s%s\n", part1, part2);
+			i+=num-1;
 		}
+		result+=String.format("%-15s%.2f%s", "Total Weight:", getTotalWeight(), " kg");
 		return result;
 	}
-	
+
 	/**
 	 * 
 	 * @return the total amount of weight in the inventory
 	 */
-	public static double getTotalWeight() {
+	public double getTotalWeight() {
 		double count = 0;
-		for(Item i:items) {
-			count+=i.getItemWeight();
+		for (Item i : items) {
+			count += i.getItemWeight();
 		}
 		return count;
 	}
-	
+
 	/**
 	 * adds an item to the list
+	 * 
 	 * @param item the item you want to add to the list
 	 */
-	public static void add(Item item) {
+	public void add(Item item) {
 		items.add(item);
-		sort();
 	}
-	
+
+	/**
+	 * adds an item to the list multiple times
+	 * 
+	 * @param item    the item to add to the list
+	 * @param nCopies the number of times to add the item
+	 */
+	public void add(Item item, int nCopies) {
+		for (int i = 0; i < nCopies; i++) {
+			items.add(item);
+		}
+	}
+
 	/**
 	 * removes the first occurrence of a target
+	 * 
 	 * @param target the item you want to remove
 	 * @return if the item was in the list and removed
 	 */
-	public static boolean removeOne(Item target) {
-		if(getIndexOf(target)==-1)
+	public boolean removeOne(Item target) {
+		if (getIndexOf(target) == -1)
 			return false;
-		
+
 		items.remove(getIndexOf(target));
 		return true;
 	}
-	
+
 	/**
 	 * removes every occurrence of a target
+	 * 
 	 * @param target the item you want to remove
 	 * @return if at least 1 item was removed from the list
 	 */
-	public static boolean removeAll(Item target) {
+	public boolean removeAll(Item target) {
 		boolean result = false;
-		
-		while(getIndexOf(target)!=-1) {
+
+		while (getIndexOf(target) != -1) {
 			removeOne(target);
 			result = true;
 		}
 		return result;
 	}
-	
+
 	/**
 	 * 
 	 * @return the size of the inventory
 	 */
-	public static int size() {
+	public int size() {
 		return items.size();
 	}
-	
+
 	/**
 	 * clear everything in the inventory - useful for switching to the next phase
 	 */
-	public static void clearInventory() {
-		while(items.size()>0) {
+	public void clearInventory() {
+		while (items.size() > 0) {
 			items.remove(0);
 		}
 	}
-	
-//	public static void Main(String [] args) {
-//		Inventory inventory = new Inventory();
-//		inventory.items.add(new Item("hi", 5.5, true));
-//		inventory.items.add(new Item("hello", 6.5, true));
-//		System.out.println(inventory);
-//		
-//	}
 
 
 
--- Zork/src/com/bayviewglen/zork/Zork.java
+++ Zork/src/com/bayviewglen/zork/Zork.java
@@ -3,8 +3,18 @@ package com.bayviewglen.zork;
 public class Zork {
 	public static void main(String[] args) {
 		
-		Game game = new Game();
-		game.play();
+		Inventory inventory = new Inventory();
+		inventory.add(new Item("hi", 5.5, true));
+		inventory.add(new Item("hello", 6.5, true));
+		inventory.add(new Item("hello", 6.5, true));
+		inventory.add(new Item("hi", 5.5, true));
+		inventory.add(new Item("hello", 7.5, true));
+		System.out.println(inventory);
+		
+	
+		
+		//Game game = new Game();
+		//game.play();
 		
 	}
 }
