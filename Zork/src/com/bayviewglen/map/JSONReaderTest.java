55dec3d82705e9ae6e073d9a9f7b2720d5a7003a
Ethan Elbaz <32203189+RC3th4n@users.noreply.github.com>
2019-04-01 10:33:30 -0400

    Updates to Map Loader

---
+++ Zork/src/com/bayviewglen/map/JSONReaderTest.java
@@ -0,0 +1,27 @@
+package com.bayviewglen.map;
+import org.json.*;
+import com.bayviewglen.zork.FileReader;
+
+public class JSONReaderTest {
+	
+	public static void main(String[] args) {
+		FileReader mapReader = new FileReader("data/jasontest.json");
+		String[] lines = mapReader.getLines();
+		String mastaboi = "";
+		
+		for (String s : lines) {
+			mastaboi += s + "\n";
+		}
+		
+		JSONObject obj = new JSONObject(mastaboi);
+		String name = obj.getString("name");
+		System.out.println(name);
+
+		JSONArray arr = obj.getJSONArray("places");
+		for (int i = 0; i < arr.length(); i++)
+		{
+			String room = arr.getJSONObject(i).getString("name");
+			System.out.println(room);
+		}
+	}
+}
--- Zork/src/com/bayviewglen/map/MapLoader.java
+++ Zork/src/com/bayviewglen/map/MapLoader.java
@@ -1,18 +1,56 @@
 package com.bayviewglen.map;
 
 import com.bayviewglen.zork.FileReader;
+import org.json.*;
 
 public class MapLoader {
 	public static void main(String[] args) {
-
+		Map map = loadMap("data/jasontest.json");
 	}
 
-	public Map loadMap(String FilePath) {
+	public static Map loadMap(String FilePath) {
 		FileReader mapReader = new FileReader(FilePath);
 		String[] lines = mapReader.getLines();
-		int[] maxCoords = new int[3];
-		int numPlaces = 0;
+		double[] maxCoords = new double[3];
+		
+		String mastaboi = ""; //change name
+		
+		for (String s : lines) {
+			mastaboi += s + "\n";
+		}
+		
+		JSONObject obj = new JSONObject(mastaboi);
+		String mapName = obj.getString("name");
+		String startCoords = obj.getString("startcoords");
+		String endCoords = obj.getString("endcoords");
+		
+		System.out.println(mapName);
+		
+		JSONArray places = obj.getJSONArray("places");
+		
+		for (int i = 0; i < places.length(); i++) {
+			String coordsString = places.getJSONObject(i).getString("coords");
+			int[] coords = readCoords(coordsString);
+			for (int j = 0; j < 3; j++) {
+				if (coords[j] > maxCoords[j]) // setting maximum coordinates
+					maxCoords[j] = coords[j];
+			}
+		}
+		
+		Map map = new Map(mapName, maxCoords[0], maxCoords[1], maxCoords[2]);
+		
+		for (int i = 0; i < places.length(); i++) {
+			if (places.getJSONObject(i).getString("type").equals("room")) {
+				
+			} else if (places.getJSONObject(i).getString("type").equals("door")) {
+				System.out.println("ITS A GIRL");
+			}
+			
+//			String room = places.getJSONObject(i).getString("coords");
+//			System.out.println(room);
+		}
 
+		/*
 		for (String line : lines) {
 			// gets the coordinates in a string
 			if (line.charAt(0) == '(') {
@@ -26,7 +64,7 @@ public class MapLoader {
 		}
 		
 		// create new map with max coords
-		Map map = new Map("map",maxCoords[0], maxCoords[1], maxCoords[2]); 
+		Map map = new Map(maxCoords[0], maxCoords[1], maxCoords[2]); 
 		
 		int lineNum = 0;
 		for (int i = 0; i < numPlaces; i++) {
@@ -35,14 +73,14 @@ public class MapLoader {
 			String keyName;
 			String line = lines[lineNum];
 			if (line.charAt(0) == '(') {
-				coords = readCoords(line);
+				int[] coords = readCoords(line);
 			}
 		}
-
+*/
 		return map;
 	}
 	
-	public int[] readCoords (String line) {
+	public static int[] readCoords (String line) {
 		line.replaceAll("[ (){]", "");
 		String[] coordsString = line.split(","); // split on comma to extract coords
 		int[] coords = new int[3];
