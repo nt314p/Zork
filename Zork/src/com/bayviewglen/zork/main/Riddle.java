30f124fecf2e7d03e42a17af836739e81e99da8b
Ben Merbaum <46571024+bmerbaum@users.noreply.github.com>
2019-04-08 17:37:30 -0400

    Riddles with chars
    You can now input a letter option (ex. option 'a', 'b', etc.). Also toString() added

--- Zork/src/com/bayviewglen/zork/main/Riddle.java
+++ Zork/src/com/bayviewglen/zork/main/Riddle.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 public class Riddle {
 	
+	final int ASCII_a = 97;
 	String question;
 	String answer;
 	
@@ -24,6 +25,16 @@ public class Riddle {
 		}
 	}
 	
+	public String toString() {
+		String str = question + "\n";
+		if(hasChoices()) {
+			for(int i = 0; i<choices.size(); i++) {
+				str+= (char)(i+ASCII_a) + ": " + choices.get(i);
+			}
+		}
+		return str;
+	}
+	
 	public boolean isAnswer(String answer) {
 		return this.answer.equals(answer);
 	}
@@ -33,6 +44,14 @@ public class Riddle {
 			return false;
 		
 		return isAnswer(choices.get(index));
+	}
+	
+	public boolean isAnswer(char answer) {
+		if(!hasChoices())
+			return false;
+		
+		int temp = (int)answer - ASCII_a;
+		return isAnswer(choices.get(temp));
 	}
 	
 	public String getQuestion() {
@@ -47,15 +66,27 @@ public class Riddle {
 		return answer;
 	}
 	
+	public int getAnswerIndex() {
+		if(!hasChoices())
+			return -1;
+		
+		return choices.indexOf(answer);
+	}
+	
+	public char getAnswerChar() {
+		int temp = getAnswerIndex();
+		if(temp == -1)
+			return (char)temp;
+		
+		return (char)(temp + ASCII_a);
+	}
+	
 	public void setAnswer(String answer) {
 		this.answer = answer;
 		if(hasChoices() && !answerInChoices())
 			choices.add(answer);
 	}
 	
-	public int getAnswerIndex() {
-		return choices.indexOf(answer);
-	}
 	
 	public ArrayList<String> getChoices(){
 		return choices;
